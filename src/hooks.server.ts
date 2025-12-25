533d957dca6ff14b9ee5e2e8d725ea966578f140
Lucas de Bona Sartor <29283682+lucasbsartor@users.noreply.github.com>
2025-12-24 23:58:15 -0300

    feat: adds user session handling in server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,8 +4,17 @@ import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 
-export const authHandle: Handle = async ({ event, resolve }) => {
+const authHandle: Handle = async ({ event, resolve }) => {
 	return svelteKitHandler({ event, resolve, auth, building });
 };
 
-export const handle = sequence(authHandle);
+const sessionHandle: Handle = async ({ event, resolve }) => {
+	const session = await auth.api.getSession({
+		headers: event.request.headers
+	});
+	event.locals.user = session?.user;
+	const response = await resolve(event);
+	return response;
+};
+
+export const handle = sequence(authHandle, sessionHandle);
