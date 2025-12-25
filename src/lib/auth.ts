f35ea295db7968f0fcf7d3cef974e98aa56fba29
Lucas de Bona Sartor <29283682+lucasbsartor@users.noreply.github.com>
2025-12-24 23:09:24 -0300

    feat: add better-auth integration and user authentication schema

---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import { db } from '$lib/server/db';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { admin } from 'better-auth/plugins';
+
+export const auth = betterAuth({
+	emailAndPassword: {
+		enabled: true
+	},
+	database: drizzleAdapter(db, {
+		provider: 'pg'
+	}),
+	plugins: [admin()]
+});
